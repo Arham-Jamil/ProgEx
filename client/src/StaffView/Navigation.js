6f4b82fb8a5d17e903de111d40b80de2ee6ae374
Dennis1415 <Dennis.Kammos@gmx.de>
2023-07-07 15:51:00 +0200

    Added Navigation page for testing

--- client/src/StaffView/Navigation.js
+++ client/src/StaffView/Navigation.js
@@ -6,6 +6,11 @@ const Navigation = () => {
     <div>
       <h2>Navigation</h2>
       <ul>
+        <h3>Client Sites</h3>
+        <li>
+          <Link to="/?tableNumber=696969">Homepage with TableNumber 696969</Link>
+        </li>
+        <h3>KitchenView Sites</h3>
         <li>
           <Link to="/ingredientsList">Ingredients List</Link>
         </li>
@@ -16,7 +21,35 @@ const Navigation = () => {
           <Link to="/editCategoryDish">Edit Category Dish</Link>
         </li>
         <li>
-          <Link to="/editCategoryDrink">Edit Category Drink</Link>
+          <Link to="/editCategoryDrinks">Edit Category Drinks</Link>
+        </li>
+        <h3>Server</h3>
+        <li>
+          <Link to="http://localhost:3001/orders">Orders</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/ordereddishes">OrderedDishes</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/ordereddrinks">OrderedDrinks</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/CategoryDish">CategoryDish</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/CategoryDrinks">CategoryDrinks</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/Extras">Extras</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/Ingredients">Ingredients</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/Drinks">Drinks</Link>
+        </li>
+        <li>
+          <Link to="http://localhost:3001/Dishes">Dishes</Link>
         </li>
       </ul>
     </div>
