b1dacc565a9b1fc5aa2d02887d61e5aa75737b58
Dennis1415 <Dennis.Kammos@gmx.de>
2023-07-11 21:00:47 +0200

    added

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+import ServerCalled from '../callServer/ServerCalled';
+import HomePageNav from '../meals/HomePageNav';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      {/* Content of the footer */}
+      
+      {/* <HomePageNav/> */}
+
+    <ServerCalled/>
+
+
+    </footer>
+  );
+};
+
+export default Footer;
